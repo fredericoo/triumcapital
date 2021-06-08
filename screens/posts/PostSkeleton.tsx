4bacf02e6c0d88a8ba37183a8c8746969c3115ab
Frederico Batista <rincofrederico@gmail.com>
2021-06-08 22:02:58 +0100

    style: refine skeleton

--- screens/posts/PostSkeleton.tsx
+++ screens/posts/PostSkeleton.tsx
@@ -3,6 +3,7 @@ import { Stack, Skeleton, SkeletonText } from "@chakra-ui/react";
 const PostSkeleton: React.FC = () => (
 	<Stack>
 		<Skeleton width="100%" height="0" paddingBlockEnd="100%" />
+		<Skeleton height=".6em" />
 		<Skeleton height="1.2em" />
 		<SkeletonText mt="4" noOfLines={4} spacing="4" />
 	</Stack>
