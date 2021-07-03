1616f71b7f88e3ae798bd0522990e6e87a7886c2
Frederico Batista <rincofrederico@gmail.com>
2021-07-03 22:34:25 +0100

    refactor: move test to screens

--- src/screens/home/App.test.tsx
+++ src/screens/home/App.test.tsx
@@ -1,9 +1,9 @@
 import { render } from '@testing-library/react';
-import Home from '@/pages';
+import HomeScreen from '@/screens/home';
 
 describe('Home page', () => {
   it('does not render without data', () => {
-    const { container } = render(<Home doc={undefined} posts={[]} />);
+    const { container } = render(<HomeScreen data={undefined} posts={[]} />);
     expect(container.innerHTML).toBeNull;
   });
 });
