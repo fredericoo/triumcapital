44f72ef8b92bb3ca392eef79886dc14e27da05f4
Frederico Batista <rincofrederico@gmail.com>
2021-07-04 14:37:26 +0100

    feat: implement favicon

--- src/components/SEO/index.tsx
+++ src/components/SEO/index.tsx
@@ -31,6 +31,7 @@ const SEO: React.FC<SEO> = props => {
 
       <meta property="og:title" content={tabTitle} />
       <meta property="og:description" content={tabDesc} />
+      <meta name="description" content={tabDesc} />
 
       <link rel="canonical" href={`${constants.info.url}${asPath}`} />
       <meta property="og:url" content={`${constants.info.url}${asPath}`} />
@@ -43,16 +44,15 @@ const SEO: React.FC<SEO> = props => {
 
       <meta property="og:image" content={props.image || '/favicon.png'} />
 
-      <link rel="icon" href="/favicon.svg" />
-      <link rel="mask-icon" href="/favicon.svg" color="#000000" />
-      <meta name="msapplication-TileImage" content="/favicon.png" />
-      <link rel="apple-touch-icon" href="/img/matula-web-app.png" sizes="512x512" />
+      <link rel="icon" href="/img/favicon.svg" />
+      <link rel="mask-icon" href="/img/favicon.svg" color="#000000" />
+      <meta name="msapplication-TileImage" content="/img/favicon.png" />
+      <link rel="apple-touch-icon" href="/img/favicon.png" sizes="512x512" />
       <link rel="icon" href="/favicon.png" sizes="512x512" />
 
       {/* WEB APP */}
-      <link rel="manifest" href="/manifest.json" />
       <meta name="theme-color" content="#f0f0f0" />
-      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-capable" content="no" />
       <meta
         name="apple-mobile-web-app-status-bar-style"
         // content="default"
